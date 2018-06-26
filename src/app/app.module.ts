e6bafdfc51676dc7ccdfd960d1fdde682a086bb4
lkepka <ludovic.kepka@ca-titres.fr>
2018-06-26 12:19:17 +0200

    essai 1000 cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {CardsPage} from "../pages/cards/cards";
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    CardsPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +25,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    CardsPage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/cards/cards.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the CardsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-cards',
+  templateUrl: 'cards.html',
+})
+export class CardsPage {
+
+  public cards: Array<{name:string, content:string}> = new Array<{name:string,content:string}>();
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    for(let i=0;i<1000;i++) {
+      this.cards.push({ name:"name "+i, content:"content "+i});
+    }
+  }
+
+}
